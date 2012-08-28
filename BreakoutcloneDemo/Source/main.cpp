946d046e15d7772c1cc7841ce12322db196952fe
Timothy Carlisle <gwa2100@gmail.com>
2012-08-28 00:39:43 -0400

    BreakoutCloneDemo Updated
    Added ability to display basics. Demo in work.

--- BreakoutcloneDemo/Source/main.cpp
+++ BreakoutcloneDemo/Source/main.cpp
@@ -1,17 +1,23 @@
-#include <iostream>
 #include "AdamantEngine.h"
 
-using namespace std;
-
-class CApp2 : public CApp
+int InitPlayer(CApp* app, Sprite* pl)
 {
 
-};
-
+    pl->usesCollision = true;
+    pl->collisionRect = app->RectFiller(10,10,20,40);
+    pl->Render_Surf = app->LoadSprite("file.bmp");
+    pl->usesInput = true;
+    pl->usesRender = true;
+    app->BindSprite(pl);
+    return 0;
+}
 
 
 int main(int argc, char* argv[])
 {
-    CApp2 theApp;
+    //declare Sprite
+    Sprite player;
+    CApp theApp;
+    InitPlayer(&theApp, &player);
     return theApp.OnExecute();
 }
