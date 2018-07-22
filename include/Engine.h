a8c1d12dd24c355b07897ca2559a3c8bf0a8651a
Timothy Carlisle <gwa2100@gmail.com>
2018-07-22 16:37:04 -0500

    Initial Shell of Engine Class

--- include/Engine.h
+++ include/Engine.h
@@ -1,6 +1,17 @@
-fgf#ifndef ENGINE_H
+#ifndef ENGINE_H
 #define ENGINE_H
 
+#include "constants.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_events.h>
+#include <vector>
+//#include "TileEngine.h"
+#include "ATimer.h"
+//#include "GameObjectPtrVector.h"
+#include <stdint.h>
+
+using std::vector;
 
 class Engine
 {
@@ -10,9 +21,14 @@ class Engine
         /** Default destructor */
         virtual ~Engine();
 
-    protected:
+        int Execute();
+        bool Init();
+        //void Bind(GameObject* pBindMe);
+
 
     private:
+        ATimer m_timer;
+        bool m_bRunning;
 };
 
 #endif // ENGINE_H
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -1,8 +1,27 @@
 #include "Engine.h"
 
+//Z Comparison (DEPTH)
+//bool operator < (const CGameObject& left, const CGameObject& right)
+//{
+//	if  (left.GetPosition().z < right.GetPosition().z) return true;
+//	return false;
+//}
+
+
 Engine::Engine()
+ : m_bRunning( true )
+ , m_pSurfDisplay(NULL)
+ , m_uPrevTime(0)
+ , m_uCurrTime(0)
+ , m_uAccTime(0)
+ , m_nGravRate(0)
+ , m_nGravFrame(0)
+ , m_bJump(false)
+ , m_bAlreadyJump(false)
+ , m_nJumpTime(0)
+ , m_nJumpPower(0)
+
 {
-    //ctor
 }
 
 Engine::~Engine()
