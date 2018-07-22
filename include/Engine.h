326cd7172fb8ea2b469bb7d0574fe891a2e4a5c3
Timothy Carlisle <gwa2100@gmail.com>
2018-07-22 15:43:31 -0500

    Cleaned Up to start fresh
    Going to be going through the old code base and the new code base.  Redoing most of it.

---
+++ include/Engine.h
@@ -0,0 +1,18 @@
+fgf#ifndef ENGINE_H
+#define ENGINE_H
+
+
+class Engine
+{
+    public:
+        /** Default constructor */
+        Engine();
+        /** Default destructor */
+        virtual ~Engine();
+
+    protected:
+
+    private:
+};
+
+#endif // ENGINE_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+    return 1;
+}
