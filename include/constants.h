a2ee66dae210829a10a6d54e342b497641dcb2f7
Timothy Carlisle <gwa2100@gmail.com>
2018-07-22 16:36:46 -0500

    Created Constants.h for holding consts

---
+++ include/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+
+//Define Constants and global space functions in this file for inclusion everywhere.
+ const int DEFAULT_FRAMECAP = 60;
+
+
+#endif // CONSTANTS_H_INCLUDED
